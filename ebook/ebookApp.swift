9d9f6decf62ea0403f7a4d17b118d75f9803c516
Benson <benson.yeh.phone@gmail.com>
2023-10-18 10:59:27 +0800

    Initial Commit

---
+++ ebook/ebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookApp.swift
+//  ebook
+//
+//  Created by 李青芬 on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
